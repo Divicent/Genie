32f3ead647c727ecd975d42b8c6afd136d279547
rusith <srlsooriyagoda@gmail.com>
2017-09-06 02:50:56 +0530

    Change A

--- travis-beforedeploy.sh
+++ travis-beforedeploy.sh
@@ -2,3 +2,4 @@
 zip -r genieCLI_win-x64-${TRAVIS_TAG}.zip /home/travis/build/rusith/Genie/GenieCLI/bin/release/netcoreapp1.1/win10-x64/publish
 zip -r genieCLI_linux-x64-${TRAVIS_TAG}.zip /home/travis/build/rusith/Genie/GenieCLI/bin/release/netcoreapp1.1/ubuntu.16.10-x64/publish
 zip -r genieCLI_osx-x64-${TRAVIS_TAG}.zip /home/travis/build/rusith/Genie/GenieCLI/bin/release/netcoreapp1.1/osx.10.11-x64/publish
+echo "${PWD}"
