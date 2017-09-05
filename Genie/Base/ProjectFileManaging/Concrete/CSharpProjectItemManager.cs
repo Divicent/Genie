0fc3c1956990afd6f86b24093d0ee712b571964d
rusith <srlsooriyagoda@gmail.com>
2017-09-06 02:25:47 +0530

    CSharp Project File Manager Complete Implementation

--- Genie/Base/ProjectFileManaging/Concrete/CSharpProjectItemManager.cs
+++ Genie/Base/ProjectFileManaging/Concrete/CSharpProjectItemManager.cs
@@ -10,50 +10,69 @@ using Genie.Base.ProjectFileManaging.Abstract;
 
 namespace Genie.Base.ProjectFileManaging.Concrete
 {
-    internal class CSharpProjectItemManager : IProjectItemManager
+  internal class CSharpProjectItemManager : IProjectItemManager
+  {
+    public void Process(string projectFilePath, List<string> files, IProcessOutput output)
     {
-        public void Process(string projectFilePath, List<string> files, IProcessOutput output)
+      try
+      {
+        if (string.IsNullOrWhiteSpace(projectFilePath) || !File.Exists(projectFilePath))
         {
-            output.WriteInformation("Processing project file.");
-            try
-            {
-                if (!File.Exists(projectFilePath))
-                {
-                    return;
-                }
-
-
-                var document = new XmlDocument();
-                document.LoadXml(File.ReadAllText(projectFilePath));
-
-                var compiles = document.GetElementsByTagName("Compile");
-                foreach (XmlNode compile in compiles)
-                {
-                    compile.RemoveAll();
-                    var parent = compile.ParentNode;
-                    if (parent.ChildNodes.Count < 1)
-                    {
-                        parent.RemoveAll();
-                    }
-                }
-
-                var root = document.CreateNode(XmlNodeType.Element, "ItemGroup", null);
-                document.AppendChild(root);
-
-                foreach (var file in files)
-                {
-                    var node = document.CreateNode(XmlNodeType.Element, "Compile", null);
-                    node.Value = file;
-                    root.AppendChild(node);
-                }
-
-                File.WriteAllText(projectFilePath, document.OuterXml);
-            }
-            catch (Exception e)
-            {
-                throw new Exception("Unable to process file.", e);
-            }
-            output.WriteSuccess("Project file processed.");
+          return;
         }
+
+        output.WriteInformation("Processing project file.");
+        var document = new XmlDocument();
+        document.LoadXml(File.ReadAllText(projectFilePath));
+        var compiles = document.GetElementsByTagName("Compile");
+        var torRemove = new List<XmlNode>();
+        foreach (XmlNode compile in compiles)
+        {
+          if (compile != null && compile.Attributes != null)
+          {
+            var include = compile.Attributes["Include"]?.Value;
+            if (!string.IsNullOrWhiteSpace(include) && (include.StartsWith("Dapper") || include.StartsWith("Infrastructure")))
+            {
+              torRemove.Add(compile);
+            }
+          }
+        }
+
+        foreach (var tr in torRemove)
+        {
+          var parent = tr.ParentNode;
+          parent.RemoveChild(tr);
+          if (parent.ChildNodes.Count < 1)
+          {
+            parent.ParentNode.RemoveChild(parent);
+          }
+        }
+
+        var root = document.CreateElement("ItemGroup", null);
+        foreach (var file in files)
+        {
+          var node = document.CreateElement("Compile", null);
+          var include = document.CreateAttribute("Include");
+          include.Value = file;
+          node.Attributes.Append(include);
+          root.AppendChild(node);
+        }
+
+        document.DocumentElement.AppendChild(root);
+
+        var xmlWriterSettings = new XmlWriterSettings { Indent = true };
+        var sb = new StringBuilder();
+        using (var xw = XmlWriter.Create(sb, xmlWriterSettings))
+        {
+          document.WriteContentTo(xw);
+        }
+        File.WriteAllText(projectFilePath, sb.Replace("xmlns=\"\"", "").ToString());
+      }
+      catch (Exception e)
+      {
+        throw new Exception("Unable to process file.", e);
+      }
+      output.WriteSuccess("Project file processed.");
     }
+  }
 }
