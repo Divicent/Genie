b471b476188a62e449ed12578d97378f47858beb
Shanaka Rusith <srlsooriyagoda@gmail.com>
2017-09-05 21:48:30 +0530

    Chanegs D

--- travis-aftersuccess.sh
+++ travis-aftersuccess.sh
@@ -1,7 +1,2 @@
 #!/usr/bin/env bash
-dir
-[ -f /home/travis/build/rusith/Genie/GenieCLI/bin/release/netcoreapp1.1/win10-x64/GenieCLI.exe ] && echo "Exe file exists" || echo "Exe file does not exist"
 zip -r genieCLI_win-x64.zip /home/travis/build/rusith/Genie/GenieCLI/bin/release/netcoreapp1.1/win10-x64
-dir
-echo "Script executed from: ${PWD}"
-echo "Travis Build: ${TRAVIS_BUILD_DIR}"
