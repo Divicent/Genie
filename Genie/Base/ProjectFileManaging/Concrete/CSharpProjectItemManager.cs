2af27c779958b042f12e58f89d2af866ec572f9b
Shanaka Rusith <srlsooriyagoda@gmail.com>
2017-09-05 23:34:31 +0530

    C# Project File Manager Implementation

--- Genie/Base/ProjectFileManaging/Concrete/CSharpProjectItemManager.cs
+++ Genie/Base/ProjectFileManaging/Concrete/CSharpProjectItemManager.cs
@@ -1,32 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml;
 using Genie.Base.ProcessOutput.Abstract;
 using Genie.Base.ProjectFileManaging.Abstract;
 
 namespace Genie.Base.ProjectFileManaging.Concrete
 {
-    internal class CSharpProjectItemManager: IProjectItemManager
+    internal class CSharpProjectItemManager : IProjectItemManager
     {
         public void Process(string projectFilePath, List<string> files, IProcessOutput output)
         {
             output.WriteInformation("Processing project file.");
             try
             {
-                // var projectCollection = new ProjectCollection();
-                // var project = projectCollection.LoadProject(projectFilePath);
+                if (!File.Exists(projectFilePath))
+                {
+                    return;
+                }
 
-                // foreach (var projectItem in project.GetItems("Compile").ToList())
-                // {
-                //     if (projectItem.EvaluatedInclude.StartsWith("Dapper") || projectItem.EvaluatedInclude.StartsWith("Infrastructure"))
-                //     {
-                //         project.RemoveItem(projectItem);
-                //     }
-                // }
-                // foreach (var file in files)
-                //     project.AddItem("Compile", file);
 
-                // project.Save();
+                var document = new XmlDocument();
+                document.LoadXml(File.ReadAllText(projectFilePath));
+
+                var compiles = document.GetElementsByTagName("Compile");
+                foreach (XmlNode compile in compiles)
+                {
+                    compile.RemoveAll();
+                    var parent = compile.ParentNode;
+                    if (parent.ChildNodes.Count < 1)
+                    {
+                        parent.RemoveAll();
+                    }
+                }
+
+                var root = document.CreateNode(XmlNodeType.Element, "ItemGroup", null);
+                document.AppendChild(root);
+
+                foreach (var file in files)
+                {
+                    var node = document.CreateNode(XmlNodeType.Element, "Compile", null);
+                    node.Value = file;
+                    root.AppendChild(node);
+                }
+
+                File.WriteAllText(projectFilePath, document.OuterXml);
             }
             catch (Exception e)
             {
