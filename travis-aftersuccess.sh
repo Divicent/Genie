67e908a10d076f93c560752e16840dddefa19f8e
Shanaka Rusith <srlsooriyagoda@gmail.com>
2017-09-05 21:33:12 +0530

    Chanegs B

--- travis-aftersuccess.sh
+++ travis-aftersuccess.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
-dotnet publish -c release -r win10-x64
 dir
-[ -f GenieCLI/bin/Release/netcoreapp1.1/win10-x64/publish/GenieCLI.exe ] && echo "Exe file exists" || echo "Exe file does not exist"
-zip -r genieCLI_win-x64.zip GenieCLI/bin/Release/netcoreapp1.1/win10-x64/publish
+[ -f ./GenieCLI/bin/Release/netcoreapp1.1/win10-x64/publish/GenieCLI.exe ] && echo "Exe file exists" || echo "Exe file does not exist"
+zip -r genieCLI_win-x64.zip ./GenieCLI/bin/Release/netcoreapp1.1/win10-x64/publish
 dir
 echo "Script executed from: ${PWD}"
 echo "Travis Build: ${TRAVIS_BUILD_DIR}"
--- travis-build.sh
+++ travis-build.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 dotnet restore GenieCLI 
 dotnet build GenieCLI
+dotnet publish GenieCLI -c release -r win10-x64
