1706d85c1d0e05146ca4c0e12c71d5c0e496f98c
Shanaka Rusith <srlsooriyagoda@gmail.com>
2017-09-05 22:37:03 +0530

    Travis Build Change

--- travis-aftersuccess.sh
+++ travis-aftersuccess.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-zip -r genieCLI_win-x64.zip /home/travis/build/rusith/Genie/GenieCLI/bin/release/netcoreapp1.1/win10-x64
+zip -r genieCLI_win-x64-${TRAVIS_TAG}.zip /home/travis/build/rusith/Genie/GenieCLI/bin/release/netcoreapp1.1/win10-x64
+zip -r genieCLI_linux-x64-${TRAVIS_TAG}.zip /home/travis/build/rusith/Genie/GenieCLI/bin/release/netcoreapp1.1/ubuntu.16.10-x64
+
