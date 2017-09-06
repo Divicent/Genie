712edcfad8882ee290fdf5a10c305f8210fbebca
Shanaka Rusith <srlsooriyagoda@gmail.com>
2017-09-06 10:59:16 +0530

    BeforeDeploy Changes

--- travis-beforedeploy.sh
+++ travis-beforedeploy.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
-zip -r genieCLI_win-x64-${TRAVIS_TAG}.zip ${TRAVIS_BUILD_DIR}/GenieCLI/bin/release/netcoreapp1.1/win10-x64/publish
-zip -r genieCLI_linux-x64-${TRAVIS_TAG}.zip ${TRAVIS_BUILD_DIR}/GenieCLI/bin/release/netcoreapp1.1/ubuntu.16.10-x64/publish
-zip -r genieCLI_osx-x64-${TRAVIS_TAG}.zip ${TRAVIS_BUILD_DIR}/GenieCLI/bin/release/netcoreapp1.1/osx.10.11-x64/publish
+cd ${TRAVIS_BUILD_DIR}/GenieCLI/bin/release/netcoreapp1.1/win10-x64/publish
+zip -r ${TRAVIS_BUILD_DIR}/genieCLI_win-x64-${TRAVIS_TAG}.zip ./*
+cd ${TRAVIS_BUILD_DIR}/GenieCLI/bin/release/netcoreapp1.1/ubuntu.16.10-x64/publish
+zip -r ${TRAVIS_BUILD_DIR}/genieCLI_linux-x64-${TRAVIS_TAG}.zip ./*
+cd ${TRAVIS_BUILD_DIR}/GenieCLI/bin/release/netcoreapp1.1/osx.10.11-x64/publish
+zip -r ${TRAVIS_BUILD_DIR}/genieCLI_osx-x64-${TRAVIS_TAG}.zip ./*
+cd  ${TRAVIS_BUILD_DIR}
