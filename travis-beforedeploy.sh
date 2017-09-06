cb27e584a42836f887840b3574bda5c999404551
Shanaka Rusith <srlsooriyagoda@gmail.com>
2017-09-06 10:38:55 +0530

    Move publish to build script

--- travis-beforedeploy.sh
+++ travis-beforedeploy.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-dotnet publish /home/travis/rusith/Genie/GenieCLI -c release -r win10-x64
-dotnet publish /home/travis/rusith/Genie/GenieCLI -c release -r ubuntu.16.10-x64
-dotnet publish /home/travis/rusith/Genie/GenieCLI -c release -r osx.10.11-x64
 zip -r genieCLI_win-x64-${TRAVIS_TAG}.zip ${TRAVIS_BUILD_DIR}/GenieCLI/bin/release/netcoreapp1.1/win10-x64/publish
 zip -r genieCLI_linux-x64-${TRAVIS_TAG}.zip ${TRAVIS_BUILD_DIR}/GenieCLI/bin/release/netcoreapp1.1/ubuntu.16.10-x64/publish
 zip -r genieCLI_osx-x64-${TRAVIS_TAG}.zip ${TRAVIS_BUILD_DIR}/GenieCLI/bin/release/netcoreapp1.1/osx.10.11-x64/publish
--- travis-build.sh
+++ travis-build.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 dotnet restore GenieCLI 
 dotnet build GenieCLI
+dotnet publish GenieCLI -c release -r win10-x64
+dotnet publish GenieCLI -c release -r ubuntu.16.10-x64
+dotnet publish GenieCLI -c release -r osx.10.11-x64
