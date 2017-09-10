8e0a64e1406fa0626a2d1b4b45fd7e5ce3e65b75
Shanaka Rusith <srlsooriyagoda@gmail.com>
2017-09-10 08:43:06 +0530

    New Models
    Moved private models of SqlServerSchemaReder to the Model Namespace as internal classes

---
+++ Genie/Base/Reading/Concrete/Models/DatabaseParameter.cs
@@ -0,0 +1,11 @@
+namespace Genie.Base.Reading.Concrete.Models
+{
+    internal class DatabaseParameter
+    {
+        public string Procedure { get; set; }
+        public string Name { get; set; }
+        public string DataType { get; set; }
+    }
+
+
+}
