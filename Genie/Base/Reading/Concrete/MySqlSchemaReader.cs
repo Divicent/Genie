59dfba7f2712db3a2daa0e10d79bbf436ed4e94d
Shanaka Rusith <srlsooriyagoda@gmail.com>
2017-09-10 06:41:23 +0530

    Basic MySqlReader implementation

--- Genie/Base/Reading/Concrete/MySqlSchemaReader.cs
+++ Genie/Base/Reading/Concrete/MySqlSchemaReader.cs
@@ -1,14 +1,34 @@
 using Genie.Base.Configuration.Abstract;
 using Genie.Base.ProcessOutput.Abstract;
 using Genie.Base.Reading.Abstract;
+using MySql.Data.MySqlClient;
 
 namespace  Genie.Base.Reading.Concrete 
 {
     internal class MySqlSchemaReader : IDatabaseSchemaReader
     {
+
         public IDatabaseSchema Read(IConfiguration configuration, IProcessOutput output)
         {
-            throw new System.NotImplementedException();
+            var  connection = GetConnection(configuration.ConnectionString);
+            try
+            {
+
+            }
+            catch
+            {
+
+            }
+            finally 
+            {
+                connection.Close();
+                connection.Dispose();
+            }
+        }
+
+        private MySqlConnection GetConnection(string connectionString) 
+        {
+            return new MySqlConnection(connectionString);
         }
     }
 }
