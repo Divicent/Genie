84f7f800fd368b82d58d7669757b0d04efa353e2
Shanaka Rusith <srlsooriyagoda@gmail.com>
2017-10-14 14:09:44 +0530

    Remove debug changes

--- GenieCLI/Program.cs
+++ GenieCLI/Program.cs
@@ -3,9 +3,9 @@ using System.Linq;
 
 namespace GenieCLI
 {
-    internal class Program
+    public class Program
     {
-        private static void Main(string[] args)
+        public static void Main(string[] args)
         {
             var fileName = "genieSettings.json";
             var output = new ProcessOutput();
