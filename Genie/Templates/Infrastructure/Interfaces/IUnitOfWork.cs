f3d8890e2a5d6cd8ee19a3a37a6396fd7f50fa00
rusith <srlsooriyagoda@gmail.com>
2017-10-15 20:58:55 +0530

    Comments on IUnitOfWork

--- Genie/Templates/Infrastructure/Interfaces/IUnitOfWork.cs
+++ Genie/Templates/Infrastructure/Interfaces/IUnitOfWork.cs
@@ -24,12 +24,22 @@ namespace Genie.Core.Templates.Infrastructure.Interfaces
 
             foreach (var relation in _schema.Relations)
             {
-                relations.AppendLine($@"      I{relation.Name}Repository {relation.Name}Repository {{ get; }}");
+                relations.AppendLine($@"
+      /// <summary>
+      /// The {relation.Name} repository that belongs to this unit of work.
+      /// </summary>
+      I{relation.Name}Repository {relation.Name}Repository {{ get; }}
+");
             }
 
             foreach (var view in _schema.Views)
             {
-                views.AppendLine($@"      I{view.Name}Repository {view.Name}Repository {{ get; }}");
+                views.AppendLine($@"
+      /// <summary>
+      /// The {view.Name} repository (read only) that belongs to this unit of work.
+      /// </summary>
+      I{view.Name}Repository {view.Name}Repository {{ get; }}
+");
             }
 
             L($@"
@@ -42,20 +52,33 @@ using {GenerationContext.BaseNamespace}.Infrastructure.Repositories.Abstract;
 
 namespace {GenerationContext.BaseNamespace}.Infrastructure.Interfaces
 {{
+  /// <summary>
+  /// A unit of work is a collection of operations on a data source. operations could be updates, deletes, inserts
+  /// </summary>
 	public interface IUnitOfWork : IDisposable
     {{
 
+        /// <summary>
+        /// Starts a transaction on this unit of work
+        /// </summary>
+        /// <returns>A new transaction</returns>
         IDbTransaction BeginTransaction();
 
-	    void AddOp(IOperation operation);
+	      void AddOp(IOperation operation);
         void AddObj(BaseModel obj);
 
 {relations}
 
 {views}
 
-		IProcedureContainer Procedures {{ get; }}
+        /// <summary>
+        /// The procedure container that belongs to this unit of work
+        /// </summary>
+		    IProcedureContainer Procedures {{ get; }}
         
+        /// <summary>
+        /// This will commit all changes to the data source performed in this unit of work one by one. including all tracked changes of the objects retrieved from this unit of work
+        /// </summary>
         void Commit();
     }}
 }}
