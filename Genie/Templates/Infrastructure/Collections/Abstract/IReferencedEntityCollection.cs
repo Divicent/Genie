a889eab686ec725065f580ad1bb74cdf5883cc9e
rusith <srlsooriyagoda@gmail.com>
2017-10-15 21:04:53 +0530

    Comments on Referenced Entity collection

--- Genie/Templates/Infrastructure/Collections/Abstract/IReferencedEntityCollection.cs
+++ Genie/Templates/Infrastructure/Collections/Abstract/IReferencedEntityCollection.cs
@@ -23,10 +23,18 @@ using {GenerationContext.BaseNamespace}.Infrastructure.Models.Concrete;
 
 namespace {GenerationContext.BaseNamespace}.Infrastructure.Collections.Abstract
 {{
+    /// <summary>
+    /// Collection of data models retrieved from an object. as a reference collection
+    /// </summary>
+    /// <typeparam name=""T"">Type of object</typeparam>
     public interface IReferencedEntityCollection<T> : IEnumerable<T> where T: BaseModel
-	{{
-		void Add(T entityToAdd);
-	}}
+	  {{
+      /// <summary>
+      /// Add an object to the collection. this will also update the parent objects related value which the collection is retrieved from. this will not add this object to the data source.
+      /// </summary>
+      /// <param name=""entityToAdd"">entity to add </param>
+      void Add(T entityToAdd);
+	  }}
 }}
 
 ");
