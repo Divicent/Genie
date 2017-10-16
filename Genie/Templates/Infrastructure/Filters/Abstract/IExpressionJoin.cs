a70cad660cd8209a6fd2d216ecf669c1e29f5042
rusith <srlsooriyagoda@gmail.com>
2017-10-16 20:17:51 +0530

    Comments on Expression join

--- Genie/Templates/Infrastructure/Filters/Abstract/IExpressionJoin.cs
+++ Genie/Templates/Infrastructure/Filters/Abstract/IExpressionJoin.cs
@@ -20,18 +20,37 @@ namespace Genie.Core.Templates.Infrastructure.Filters.Abstract
 
 namespace {GenerationContext.BaseNamespace}.Infrastructure.Filters.Abstract
 {{
+
+    /// <summary>
+    /// An expression join is a helper that helps to continue a query in a builder-like pattern
+    /// </summary>
+    /// <typeparam name=""T"">Type of the filter context</typeparam>
+    /// <typeparam name=""TQ"">Type of the query context</typeparam>
     public interface IExpressionJoin<out T, out TQ> where T : IFilterContext
     {{
-        T And {{ get; }}
-        T Or {{ get; }}
-        TQ Filter();
+
         /// <summary>
-        /// Start Parenthesizes
+        /// Adds an and condition to the query
+        /// </summary>
+        T And {{ get; }}
+
+        /// <summary>
+        /// Adds an or condition to the query
+        /// </summary>
+        T Or {{ get; }}
+        
+        /// <summary>
+        /// Completes the filter context and returns to the current query context
+        /// </summary>
+        TQ Filter();
+        
+        /// <summary>
+        /// Starts Parenthesizes
         /// </summary>
         IExpressionJoin<T, TQ> Start {{ get; }}
 
         /// <summary>
-        /// End Parenthesizes
+        /// Ends Parenthesizes
         /// </summary>
         IExpressionJoin<T, TQ> End {{ get; }}
     }}
