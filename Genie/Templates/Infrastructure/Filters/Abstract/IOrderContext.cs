4ab0ead8d94aa95457a5a15bf1c856000810780a
rusith <srlsooriyagoda@gmail.com>
2017-10-17 07:23:49 +0530

    Comments on order element

--- Genie/Templates/Infrastructure/Filters/Abstract/IOrderContext.cs
+++ Genie/Templates/Infrastructure/Filters/Abstract/IOrderContext.cs
@@ -21,10 +21,25 @@ namespace Genie.Core.Templates.Infrastructure.Filters.Abstract
 using System.Collections.Generic;
 namespace {GenerationContext.BaseNamespace}.Infrastructure.Filters.Abstract
 {{
-	public interface IOrderContext
+    /// <summary>
+    /// An order context is used to build the order by clause of the target query
+    /// </summary>
+	  public interface IOrderContext
     {{
+        /// <summary>
+        /// Adds an and condition
+        /// </summary>
         void And();
+        
+        /// <summary>
+        /// Adds a custom expression
+        /// </summary>
+        /// <param name=""expression"">Expression to apply</param>
         void Add(string expression);
+        
+        /// <summary>
+        /// Current expressions as a Queue
+        /// </summary>
         Queue<string> GetOrderExpressions();
     }}
 }}
--- Genie/Templates/Infrastructure/Filters/Abstract/IOrderElement.cs
+++ Genie/Templates/Infrastructure/Filters/Abstract/IOrderElement.cs
@@ -20,10 +20,26 @@ namespace Genie.Core.Templates.Infrastructure.Filters.Abstract
 
 namespace {GenerationContext.BaseNamespace}.Infrastructure.Filters.Abstract
 {{
+
+  /// <summary>
+  /// Helps to add complete order expressions to the target query
+  /// </summary>
+  /// <typeparam name=""T"">Type of the order context</typeparam>
+  /// <typeparam name=""TQ"">type of the query contexts</typeparam>
 	public interface IOrderElement<out T, out TQ> where T : IOrderContext
 	{{
+
+    /// <summary>
+    /// Adds an expression to the query to order the result by current attribute ascending
+    /// </summary>
+    /// <returns>A join to continue the query</returns>
 		IOrderJoin<T, TQ> Ascending();
-		IOrderJoin<T, TQ> Descending();
+		
+    /// <summary>
+    /// Adds an expression to the query to order the result by current attribute descending
+    /// </summary>
+    /// <returns>A join to continue the query</returns>
+    IOrderJoin<T, TQ> Descending();
 	}}
 }}
 
