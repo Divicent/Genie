61dd75da745a39ea076341f6785cda27986b8acd
Shanaka Rusith <srlsooriyagoda@gmail.com>
2017-11-23 15:55:11 +0530

    Added small date time

--- Genie/Tools/CommonTools.cs
+++ Genie/Tools/CommonTools.cs
@@ -48,6 +48,7 @@ namespace Genie.Core.Tools
                     return "decimal";
                 case "datetime2":
                 case "datetime":
+                case "smalldatetime":
                 case "date":
                     return "DateTime";
                 case "varchar":
