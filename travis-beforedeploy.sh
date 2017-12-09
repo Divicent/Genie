cc5ba379d189b253dd4843ac21bd3d64a5e5ad10
Shanaka Rusith <srlsooriyagoda@gmail.com>
2017-12-09 08:43:54 +0530

    Chanegd the OSX version

--- travis-beforedeploy.sh
+++ travis-beforedeploy.sh
@@ -3,6 +3,6 @@ cd ${TRAVIS_BUILD_DIR}/GenieCLI/bin/release/netcoreapp2.0/win10-x64/publish
 zip -r ${TRAVIS_BUILD_DIR}/genieCLI_win-x64-${TRAVIS_TAG}.zip ./*
 cd ${TRAVIS_BUILD_DIR}/GenieCLI/bin/release/netcoreapp2.0/ubuntu.16.10-x64/publish
 zip -r ${TRAVIS_BUILD_DIR}/genieCLI_linux-x64-${TRAVIS_TAG}.zip ./*
-cd ${TRAVIS_BUILD_DIR}/GenieCLI/bin/release/netcoreapp2.0/osx.10.11-x64/publish
+cd ${TRAVIS_BUILD_DIR}/GenieCLI/bin/release/netcoreapp2.0/osx.10.12-x64/publish
 zip -r ${TRAVIS_BUILD_DIR}/genieCLI_osx-x64-${TRAVIS_TAG}.zip ./*
 cd  ${TRAVIS_BUILD_DIR}
--- travis-build.sh
+++ travis-build.sh
@@ -3,4 +3,4 @@ dotnet restore GenieCLI
 dotnet build GenieCLI
 dotnet publish GenieCLI -c release -r win10-x64
 dotnet publish GenieCLI -c release -r ubuntu.16.10-x64
-dotnet publish GenieCLI -c release -r osx.10.11-x64
+dotnet publish GenieCLI -c release -r osx.10.12-x64
