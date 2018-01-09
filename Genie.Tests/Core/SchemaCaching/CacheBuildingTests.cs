324f7276fd6289b657d9cfdad25a25b128482817
rusith <srlsooriyagoda@gmail.com>
2018-01-09 20:44:29 +0530

    Some changes

--- Genie.Tests/Core/SchemaCaching/CacheBuildingTests.cs
+++ Genie.Tests/Core/SchemaCaching/CacheBuildingTests.cs
@@ -1,4 +1,5 @@
-﻿using Genie.Core.Models.Abstract;
+﻿using System.Collections.Generic;
+using Genie.Core.Models.Abstract;
 using Genie.Core.Models.Concrete;
 using Genie.Core.Tools;
 using Xunit;
@@ -14,6 +15,7 @@ namespace Genie.Tests.Core.SchemaCaching
         {
             var attribute = new SimpleAttribute
             {
+                
                 Name = "TheName",
                 Comment = "TheComment",
                 DataType = "string"
@@ -26,6 +28,33 @@ namespace Genie.Tests.Core.SchemaCaching
                 Comment = "TheComment",
             }), hash);
             
+        }
+
+        [Fact]
+        public void GenerateHashOfAModel()
+        {
+            IModel model = new Relation
+            {
+                Name = "TestView", 
+                Comment = "TestComment",
+                
+                Attributes = new List<IAttribute>
+                {
+                    new Attribute
+                    {
+                        Name = "Attribute1",
+                        Comment = "TheComment1",
+                        DataType = "string"
+                    },
+                    new Attribute
+                    {
+                        Name = "Attribute2",
+                        Comment = "TheComment2",
+                        DataType = "string"
+                    }
+                }
+                
+            };
         }
     }
 }
