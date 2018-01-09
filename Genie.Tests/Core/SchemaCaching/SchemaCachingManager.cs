42140ae2f73ab37e54693dbcdf821f0075913409
Shanaka Rusith <srlsooriyagoda@gmail.com>
2018-01-09 17:04:22 +0530

    Some new changes

--- Genie.Tests/Core/SchemaCaching/SchemaCachingManager.cs
+++
@@ -1,7 +0,0 @@
-﻿
-namespace Genie.Tests.Core.SchemaCaching
-{
-    class SchemaCachingManager
-    {
-    }
-}
---
+++ Genie/Base/Versioning/Concrete/GenieVersionManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Genie.Core.Base.Exceptions;
+using Genie.Core.Base.Files.Abstract;
+using Genie.Core.Base.Versioning.Abstract;
+
+namespace Genie.Core.Base.Versioning.Concrete
+{
+    public class GenieVersionManager: IVersionManager
+    {
+        private readonly IFileSystem _fileSystem;
+
+        public GenieVersionManager(IFileSystem fileSystem)
+        {
+            _fileSystem = fileSystem;
+        }
+
+        public string GetCurrentVersion()
+        {
+            var assembliLocation = _fileSystem.GetCurrentAssemblyLocation();
+            var versionFileLocation = _fileSystem.CombinePaths(assembliLocation, ".version");
+
+            if(!_fileSystem.Exists(versionFileLocation))
+               throw new GenieException($".version file not found in the location {assembliLocation}");
+
+            return _fileSystem.ReadText(versionFileLocation);
+        }
+    }
+}
