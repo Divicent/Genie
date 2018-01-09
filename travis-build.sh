1c00a18e6e43b29d5eec55c6396de21a7c2ae39e
Shanaka Rusith <srlsooriyagoda@gmail.com>
2018-01-09 16:08:07 +0530

    Version Test 1

--- travis-build.sh
+++ travis-build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 dotnet restore GenieCLI 
 dotnet build GenieCLI
+
+echo "Version : $GENIE_VERSION"
+
 dotnet publish GenieCLI -c release -r win10-x64
 dotnet publish GenieCLI -c release -r ubuntu.16.10-x64
 dotnet publish GenieCLI -c release -r osx-x64
+
