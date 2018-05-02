41b8dcb33cfaffe884d30f15ce02e4d7e4e7de01
Shanaka Rusith <srlsooriyagoda@gmail.com>
2018-05-02 18:10:48 +0530

    Core Version Update

--- Genie/Base/ProjectFileManaging/CSharpProjectItemManager.cs
+++ Genie/Base/ProjectFileManaging/CSharpProjectItemManager.cs
@@ -19,7 +19,7 @@ namespace Genie.Core.Base.ProjectFileManaging
     /// </summary>
     internal static class CSharpProjectItemManager
     {
-        private const string GenieCoreVersion = "1.0.0-beta-5";
+        private const string GenieCoreVersion = "1.0.0-beta-8";
 
 
         /// <summary>
